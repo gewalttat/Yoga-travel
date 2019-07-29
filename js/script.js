ad58f096cacdb2d0d148af2edef894eb210b08b0
gewalttat <rbhorses@mail.ru>
2019-07-29 23:20:30 +0300

    added some comments to the code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+    //получение переменных 
     let tab = document.querySelectorAll('.info-header-tab'),
     info = document.querySelector('.info-header'),
     tabContent = document.querySelectorAll('.info-tabcontent');
 
+//функция прячет весь контент, кроме первого div
     function hideTabContent(a) {
 for (let i = a; i < tabContent.length; i++) {
     tabContent[i].classList.remove('show');
@@ -13,6 +15,7 @@ for (let i = a; i < tabContent.length; i++) {
     }
     hideTabContent(1);
 
+
     function showTabContent(b) {
         if(tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
@@ -20,8 +23,10 @@ for (let i = a; i < tabContent.length; i++) {
         }
     }
 
+    //переключение между контентом, завязанное на клик. переход между контентом включает стили класса .info-header-tab
     info.addEventListener('click', function(event) {
 let target = event.target;
+//если пользователь тыкает в кнопку класса info-header-tab, запускается переход между дивами
 if (target && target.classList.contains('info-header-tab')) {
 for (let i = 0; i < tab.length; i++) {
     if(target==tab[i]) {
