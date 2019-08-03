d9f70ebee9f846e9d288933a24ec75aa0de376e6
gewalttat <rbhorses@mail.ru>
2019-08-03 22:50:05 +0300

    added modal window with fade animation

--- js/script.js
+++ js/script.js
@@ -91,4 +91,23 @@ if (t.hours <= 0 && t.minutes < 30) {
 }
 //вызов функции с id timer и дедлайном
 setClock('timer', deadline);
+
+//modal
+
+let more = document.querySelector('.more'),
+overlay = document.querySelector('.overlay'),
+close = document.querySelector('.popup-close');
+
+more.addEventListener('click', function(){
+overlay.style.display = 'block';
+this.classList.add('more-splash');
+document.body.style.overflow = 'hidden';
+});
+close.addEventListener('click', function(){
+overlay.style.display = 'none';
+more.classList.remove('move-splash');
+document.body.style.overflow = 'hidden';
+});
+
+
 });
