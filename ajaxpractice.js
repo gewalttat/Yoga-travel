2faeceb02615cc34fa221756321f0ddb1400383a
gewalttat <rbhorses@mail.ru>
2019-08-06 22:38:33 +0300

    method POST doesn't works. dont know why.

--- ajaxpractice.js
+++ ajaxpractice.js
@@ -1,3 +1,5 @@
+//код для простейшего асинхронного конвертера валют 
+//вертки не будет, делалось с примера для понимания работы кода
 let inputRub = document.getElementById('rub'),
 inputUsd = document.getElementById('usd');
 inputRub.addEventListener('input', () => {
@@ -6,5 +8,13 @@ let request = new XMLHttpRequest();
 request.open('GET', 'yoga/corrent.json');
 request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 request.send();
-
+//status
+request.addEventListener('readystatechange', function(){
+    if (request.readyState == 4 && request.status == 200) {
+        let data = JSON.parse(request.response);
+        inputUsd = inputRub.value / data.usd;
+    } else {
+        alert('something wrong...');
+    }
+})
 });
