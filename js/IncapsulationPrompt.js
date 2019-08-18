42b5f93e71b4127272a057d116ee5d2bdc64a051
gewalttat <rbhorses@mail.ru>
2019-08-18 20:39:09 +0300

    рефакторинг шпаргалки, исправление сеттера

--- js/IncapsulationPrompt.js
+++ js/IncapsulationPrompt.js
@@ -1,25 +1,39 @@
-
-function User (name, age) {
-this.name = name;
-let userAge = age;
-this.getAge = ()=> {
-    return userAge;
-};
-this.getName = ()=> {
-    return name;
-};
-this.setAge = (age) => {
-if (age != '' && age < 0 && age > 115 && age === 'number'){
-   userAge = age;
+//шпаргалка по геттерам и сеттерам
+//рефакторинг предыдущего варианта с ЕS6
+class User {
+    constructor(name, age) {
+        //назначение переменных
+        this.name = name;
+        let userAge = age;
+        //геттеры для переменных
+        this.getAge = function() {
+            return userAge;
+        };
+        this.getName = function() {
+            return name;
+        };
+        //сеттер возраста с проверкой правильности ввода
+        this.setAge = (age) => {
+            if (age > 0 && age < 115 && typeof age === 'number') {
+                userAge = age;
+            } else {
+                console.log('недопустимое значение');
+            }
+        };
+        //вызов лога введенных данных
+        this.say = function () {
+            console.log(`имя пользователя ${this.name}, возраст ${this.age}`);
+        };
+    }
 }
-};
-
-this.say = function() {
-    console.log(`имя пользователя ${this.name}, возраст ${this.userAge}`);
-};
-}
-
+//вызов функции
 let user = new User('Ivan', 25);
 console.log(user.name);
 console.log(user.userAge);
 user.say();
+//получение данных
+console.log(user.getAge());
+//оверрайд
+user.setAge(30);
+//получение переписанных данных
+console.log(user.getAge());
